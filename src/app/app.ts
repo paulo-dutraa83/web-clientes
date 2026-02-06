5b8ac9eeb6476f1a16cb1cef2058e05e67b2f11f
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-02-06 12:59:44 -0300

    Formulario de cadastro de cliente e integração com API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-clientes');
+  
+    //Criando um objeto da classe HttpClient
+    private http = inject(HttpClient);
+  
+    //Criando a estrutura do formulário
+    formulario = new FormGroup({ //formulario
+      nome: new FormControl(''), //campo nome
+      email: new FormControl(''), // campo email
+      telefone: new FormControl('') // campo telefone
+    });
+
+    //Função para realizar o cadastro do cliente
+    cadastrar() {
+
+      //Fazendo uma requisição HTTP POST para a API
+      this.http.post('http://localhost:8081/api/v1/clientes', this.formulario.value, {responseType: 'text'})
+        .subscribe((mensagem) => {
+            alert(mensagem); //Exibindo a mensagem para o usuário
+            this.formulario.reset(); //Limpando o formulário
+        }); //Aguardando o retorno da requisição
+    }
+
 }
