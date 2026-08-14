3992eaac6f06d1038f199d844cff3350f7d10284
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-08-14 13:04:12 -0300

    feat: adiciona a página de cadastro de clientes e integração com consulta de planos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-clientes');
+  
+  //Instanciando a biblioteca HTTP CLIENT
+  private http = inject(HttpClient);
+
+  //Variavel signal para armazenar o resultado da consulta
+  planos = signal<any[]>([]);
+
+  //FUnção reservada do Angular que eh executada qdo o componente eh inicializado
+  ngOnInit() {
+    //Fazendo uma chamada HTTP GET para a API de consulta de planos
+    this.http.get('http://localhost:8081/api/v1/planos')
+      .subscribe((consulta) => { //Capturando a resposta d API
+        this.planos.set(consulta as any[]); //Armazenar o resultado da consulta na variavel signal
+      });
+
+  }
+
 }
